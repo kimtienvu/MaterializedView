738e6bfc284b953f73bf38e8fd3b1996110d509b
Kim Tien Vu <45075049+kimtienvu@users.noreply.github.com>
2023-04-13 20:42:28 -0700

    Nodes with data pseudocode

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,5 +1,5 @@
-# NOTE: Need to pip install treelib
 import treelib
+import datetime
 
 def create_balanced_tree(nodes):
     """
@@ -33,19 +33,55 @@ def add_subtree(tree, nodes, parent_id, direction):
 
     # Add the middle element as a child node
     child_id = nodes[mid]
-    # Needs to be a string!
     child_id_with_direction = parent_id + direction + '_' + str(child_id)
-    tree.create_node(child_id, child_id_with_direction, parent=parent_id)
+    child1 = tree.create_node(child_id, child_id_with_direction, parent=parent_id)
+
+    # Add data to node being added
+    # NOT TESTED: getting the size, frequency of each update, and estimation for execution cost of the material view
+    '''
+    # If works, make this a function b/c root needs this too
+    cursor = connection.cursor()
+    cursor.execute("SELECT owner, segment_name, bytes FROM dba_segments WHERE segment_name = 'INSERT NAME OF MV HERE TODO' AND segment_type = 'MATERIALIZED VIEW'")
+    size_data = cursor.fetchone()
+    size = size_data[2]
+
+    cursor.execute("SELECT last_refresh_date FROM user_mviews WHERE mview_name = 'insert mv name TODO'");
+    freq_data = cursor.fetchone()
+    temp_freq = freq_data[0]
+
+    #calculate frequency of updates
+    time_elapsed = datetime.datetime.now() - last_refresh_date
+    days_elapsed = time_elapsed.days
+    frequency = 1.0 / days_elapsed
+
+    #Estimate the cost of executing a materialized view
+    cursor.execute("BEGIN DBMS_MVIEW.EXPLAIN_MVIEW('INSERT NAME OF MV HERE TODO'); END;")
+    statistics = cursor.fetchall()
+
+    # Extract execution statistics: cost is estimated cost of executing the mv, cardinality is the estimated number of rows returned by the mv
+    for row in statistics:
+        if row[0] == 'Cost':
+            cost = row[1]
+        elif row[0] == 'Cardinality':
+            num_rows = row[1]
+    '''
+    size = 1024
+    frequency = 10
+    cost = 8
+    child1.data = {"size":size, "frequency":frequency, "exec_cost":cost}
+
+    print(child1.data["size"])
 
     # Recursively add the left and right subtrees
     add_subtree(tree, nodes[:mid], child_id_with_direction, "left")
     add_subtree(tree, nodes[mid+1:], child_id_with_direction, "right")
 
-# Each node in the tree should represent a materialized view
-nodes = ["SELECT * FROM TABLEA", "SELECT * FROM TABLEB"]
+# Example usage
+nodes = ["MV1", "MV2", "MV3", "MV4", "MV5", "MV6"]
 tree = create_balanced_tree(nodes)
 tree.show()
 
-# TODO: Use dynamic programming to traverse the materialized view tree and choose the optimal one
 
-# After getting the optimal one from the tree, execute the materialized view???
+# TODO: Use dynamic programming to traverse the mv tree and calculate the optimal one
+
+# After getting optimal one from tree, execute MV?
